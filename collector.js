081d9bec886a19d38cf17861ec6e03f27d962f85
Safety-Override <88028794+Safety-Override@users.noreply.github.com>
2022-04-21 19:26:37 +0300

    Add files via upload

---
+++ collector.js
@@ -0,0 +1,104 @@
+﻿const Discord = require('discord.js');
+const bot = new Discord.Client();
+const download = require('download-file');
+const imageLib = require('./imageLib.js');
+let k = 0;
+var options = {
+    directory: "./img",
+    filename: `Hell.png`
+}
+
+bot.on('ready', () => {
+    console.log('Ready');
+});
+
+bot.on('message', async msg => {
+    if ((msg.content.includes(' collect ')) && (msg.author.id === "bot_id")) {
+        console.log("Found image.")
+        if (!msg.attachments.first()) return;
+        let url = msg.attachments.first().url;
+        await (async () => {
+            return new Promise(resolve => {
+                download(url, options, function(err) {
+                    if (err) throw err;
+                    console.log("Saved.")
+                    resolve('Saved.')
+                })
+            })
+        })()
+        imageLib('./img/Hell.png').pngToData(async function() {
+    let f = ``;
+    let that1 = this;
+    function c1(x, y) {
+        if ((that1.getPixel(x, y)[0] === 255) && (that1.getPixel(x, y)[1] === 255) && (that1.getPixel(x, y)[2] === 255) && (that1.getPixel(x, y)[3] === 255)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    that1.resize(500, 500, true);
+    let m = 0;
+    {
+        for (let j = 0; j < 4; j++) {
+            let a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            for (let i = 0; i < 16; i++)
+                await (async () => {
+                    return new Promise(resolve => {
+                        imageLib(`D:/bot/img/${i}.png`).pngToData(async function() {
+                            let that2 = this;
+                            function c2(x, y) {
+                                if (!(that2.getPixel(x, y)[0] === 255) && !(that2.getPixel(x, y)[1] === 255) && !(that2.getPixel(x, y)[2] === 255) && (that2.getPixel(x, y)[3] === 255)) {
+                                    return true
+                                } else {
+                                    return false
+                                }
+                            }
+                            function cmp(x1, y1, x2, y2) {
+                                if ((x2 == 15) && (y2 == 29)) {
+                                    if (c1(x1, y1) === c2(x2, y2)) {
+                                        return 20
+                                    } else return 0;
+                                } else {
+                                    if (c1(x1, y1) === c2(x2, y2)) {
+                                        return 1
+                                    } else return 0;
+                                }
+                            }
+                            while (!(c1(m, 12) || c1(m, 42))) {
+                                m++;
+                            }
+                            for (let x = 0; x < 16; x++) {
+                                for (let y = 0; y < 30; y++) {
+                                    a[i] = a[i] + cmp(x + m, y + 12, x, y);
+                                    resolve(i, a[i]);
+                                }
+                            }
+                        })
+                    })
+                })()
+            let n = a[0];
+            t = 0;
+            for (let b = 1; b < 16; b++)
+                if (a[b] > n) {
+                    n = a[b];
+                    t = b
+                }
+           if (t < 16) {
+                f = f + '0123456789abcdef'[t]
+            } 
+            if (f[j] == 'f') {
+                m = m + 16
+            } else
+                while ((c1(m, 12) || c1(m, 42))) {
+                    m++;
+                }
+        }
+        msg.channel.send(`.pick ${f}`);
+        k++;
+        console.log('Собрано:', k);
+    }
+});
+}
+});
+
+bot.login("your_token");
---
+++ imageLib-master/README.md
@@ -0,0 +1,50 @@
+# imageLib.js
+Module for processing and manipulation images on pure JavaScript
+
+`npm install imagelib -g`
+
+Install zlib:
+
+`npm install zlib -g`
+
+## Example
+```js
+var imageLib = require('./imageLib.js');
+
+imageLib(200, 200).create(function() {
+    var x, y;
+
+	for(x=20; x < 180; x++) {
+		for(y=20; y < 40; y++) {
+			this.setPixel(x, y, 255, 0, 0, 255); // black
+		}
+	}
+	
+	for(y=0; y < this.height; y++) {
+		this.setPixel(0, y, 255, 0, 0, 255);
+		this.setPixel(this.width-1, y, 255, 0, 0, 255);
+	}
+
+	for(x=0; x < this.width; x++) {
+		this.setPixel(x, 0, 255, 0, 0, 255);
+		this.setPixel(x, this.height-1, 255, 0, 0, 255);
+	}
+	
+	this.toPng('./images/simple.png');
+	this.toJpeg('./images/simple.jpg');
+});
+```
+
+## API
+
+#### [constructor(mixed first_argument[, int second_argument])](/documentation/en/constructor.md)
+#### [create(function callback)](/documentation/en/create.md)
+#### [jpegToData(function callback)](/documentation/en/jpegToData.md)
+#### [bmpToData(function callback)](/documentation/en/bmpToData.md)
+#### [pngToData(function callback)](/documentation/en/pngToData.md)
+#### [array getPixel(int x, int y)](/documentation/en/getPixel.md)
+#### [setPixel(int x, int y, int r, int g, int b, int a)](/documentation/en/setPixel.md)
+#### [resize(int width, int height[, boolean interpolation])](/documentation/en/resize.md)
+#### [pasteTo(resourse toImage, int x, int y[, boolean blending])](/documentation/en/pasteTo.md)
+#### [toPng(string file[, function callback])](/documentation/en/toPng.md)
+#### [toJpeg(string file[, function callback, [int quality]])](/documentation/en/toJpeg.md)
